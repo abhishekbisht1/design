7fca39319473aa84b581e3f5bffacbb499dc5c87
abhishekbisht1 <abhishek.bisht1@gmail.com>
2025-11-15 12:06:54 +0530

    Commit

--- src/com/design/cache/LRUCache.java
+++ src/com/design/cache/LRUCache.java
@@ -1,10 +1,30 @@
 package com.design.cache;
 
-public class LRUCache {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
 
+	private final int capacity;
+
+	public LRUCache(int capacity) {
+
+		super(capacity, 0.75f, true);
+		this.capacity = capacity;
+	}
+
+	@Override
+	protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+		return size() > capacity;
+	}
+
+	// Fetch from cache
+	public V getCache(K key) {
+		return super.getOrDefault(key, null);
+	}
+
+	public void putCache(K key, V value) {
+		super.put(key, value);
 	}
 
 }
